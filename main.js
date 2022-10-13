adca326d827d9e8f28f3476f5993e448f0382f58
renzorr <benaventer01@gmail.com>
2022-10-13 13:18:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const inputEmail = document.querySelector(".email");
+const alertText = document.querySelector(".alert");
+
+function ValidateEmail() {
+  const validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (!inputEmail.value.match(validRegex)) {
+    inputEmail.classList.add("warning");
+    alertText.classList.add("active");
+  } else {
+    return;
+  }
+}
